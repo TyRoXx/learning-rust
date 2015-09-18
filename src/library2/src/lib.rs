740e8770f39127350bb712cd9fe545088b00b03a
TyRoXx <tyroxxxx@gmail.com>
2015-09-18 19:49:12 +0200

    try match on Result

--- src/library2/src/lib.rs
+++ src/library2/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fs::read_dir;
+use std::path::Path;
 
 pub fn add(first: i32, second: i32) -> i32 {
 	first + second
@@ -6,4 +8,25 @@ pub fn add(first: i32, second: i32) -> i32 {
 #[test]
 fn it_works() {
 	assert!(5 == add(2, 3));
+}
+
+#[test]
+fn list_files() {
+	let mut listed = read_dir(Path::new("."));
+	match listed {
+		Ok(ref mut files) => {
+			let first = files.next().unwrap();
+			match first {
+				Ok(ref entry) => {
+					assert!(entry.path().as_path().is_relative());
+				}
+				Err(_) => {
+					unreachable!();
+				}
+			}
+		}
+		Err(_) => {
+			unreachable!();
+		}
+	}
 }
